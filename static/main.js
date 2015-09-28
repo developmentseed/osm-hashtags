bc48084e8cab43a9cf02f7bd3021b47ffa9ef664
Dylan <Dylan@developmentseed.org>
2015-09-28 13:04:06 -0400

    Tweaking style

--- static/main.js
+++ static/main.js
@@ -1,112 +1,98 @@
 /*global L, $, io, omnivore, tinysort */
-var root = 'http://hashtags.developmentseed.org';
-var southWest = L.latLng(-89, 179),
-    northEast = L.latLng(89, -179),
-    bounds = L.latLngBounds(southWest, northEast);
+var root = 'http://ludwig.local:8080';
+// var root = '';
 
 var mapboxTiles = L.tileLayer('https://api.mapbox.com/v4/devseed.24440516/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q', {
     maxZoom: 2,
     minZoom: 2,
-    maxBounds: bounds,
-    attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
+    attribution: "© <a href='https://www.mapbox.com/map-feedback/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap contributors</a>"
 });
-
 
 var map = L.map('map')
     .addLayer(mapboxTiles)
-    .setView([13.025966, -50], 2);
+    .setView([18.025966, -30], 2)
+    .setMaxBounds([ [89, -230],[-89, 230] ])
+    ;
 
 // new L.Control.Zoom({ position: 'topright' }).addTo(map);
 var socket = io.connect(root);
 
 var nextTimeline = [];
 var currentTimeline = [];
-socket.on('timeline', function (timeline) {
-  nextTimeline = timeline;
-});
-
-var renderGroup = new L.FeatureGroup().addTo(map);
-function resetUI () {
-  $('#leaderboard').empty();
-  $('#logroll').empty();
-  renderGroup.clearLayers();
-  $('#progress-bar').css('width', '0%');
-}
-
+var colors = '0,1,2,3,4,5,6,7,8,9,10'.split(',');
 var paused = false;
 var progressBarWidth = 0;
 var currentProgress = 0;
-setInterval(function () {
-  if (currentTimeline.length === 0) {
-    currentTimeline = preprocess(nextTimeline.slice(0));
-    progressBarWidth = currentTimeline.length;
-    currentProgress = 0;
-    paused = true;
-    setTimeout(function () {
-      paused = false;
-      resetUI();
-    }, 3000);
-  } else {
+
+socket.on('timeline', function (timeline) {
+  nextTimeline = JSON.parse(timeline);
+});
+
+function reset () {
+  $('#leaderboard').empty();
+  $('#logroll').empty();
+  $('#progress-bar').css('width', '0%');
+
+  currentTimeline = nextTimeline.slice(0);
+  progressBarWidth = currentTimeline.length;
+  currentTimeline.unshift('LAST');
+  currentProgress = 0;
+
+  // Reinitialize color pool
+  colors = '0,1,2,3,4,5,6,7,8,9,10'.split(',');
+}
+
+$.get(root + '/timeline', function (timeline) {
+  nextTimeline = JSON.parse(timeline);
+  reset();
+  $('#spinner').hide();
+  setInterval(function () {
     if (!paused) {
       render(currentTimeline.pop());
     }
-  }
-}, 400);
+  }, 400);
+});
 
 var options = {
   lng: function (d) { return d[0]; },
   lat: function (d) { return d[1]; },
-  duration: 1000
+  duration: 2000
 };
 var pingLayer = L.pingLayer(options).addTo(map);
 pingLayer.radiusScale().range([2, 18]);
 pingLayer.opacityScale().range([1, 0]);
 
-function preprocess (currentTimeline) {
-  console.log(currentTimeline);
-  var similar = {};
-  var retTimeline = [];
-  currentTimeline.forEach(function (element) {
-    var hashtag = element[2];
-    var feature = (element[0].startsWith('P')) ? 'building' : 'way';
-    var time = element[1];
-    if (!similar.count) {
-      similar.feature = feature;
-      similar.hashtag = hashtag;
-      similar.time = time;
-      similar.last = element[0];
-      similar.count = 1;
-    } else {
-      if (hashtag === similar.hashtag &&
-          time === similar.time &&
-            feature === similar.feature) {
-        similar.count += 1;
-      } else {
-        retTimeline.push(similar);
-        similar = {};
-      }
-    }
-  });
-  if (similar.count) {
-    retTimeline.push(similar);
-  }
-  return retTimeline;
-}
+var colorMap = {
+  '0': '#ffffff',
+  '1': '#8dd3c7',
+  '2': '#F2E855',
+  '3': '#bebada',
+  '4': '#F2695A',
+  '5': '#6CB7EB',
+  '6': '#fdb462',
+  '7': '#b3de69',
+  '8': '#FA89B1',
+  '9': '#d9d9d9',
+  '10': '#bc80bd'
+};
 
 function render (element) {
+  if (element === 'LAST') {
+    paused = true;
+    setTimeout(function () {
+      paused = false;
+      reset();
+    }, 3000);
+    return;
+  }
 
   var logroll = $('#logroll');
   var leaderboard = $('#leaderboard');
 
   var timecode = new Date(Date.parse(element.time));
-  var date = timecode.getUTCHours() + ':' + timecode.getUTCMinutes();
+  var date = timecode.getHours() + ':' + timecode.getMinutes();
 
-  logroll.prepend('<div class="logroll-item"><i>' +
-                  date + '</i> - ' +
-                  element.count + ' ' + element.feature + '(s) -' +
-                  element.hashtag + '</div>');
   var center = omnivore.wkt.parse(element.last).getBounds().getCenter();
-  pingLayer.ping([center.lng, center.lat], 'red');
 
   currentProgress += 1;
   $('#progress-bar').css('width', (100 * currentProgress / progressBarWidth) + '%');
@@ -116,17 +102,86 @@ function render (element) {
     el = $('<li>' + '#' + element.hashtag + '</li>');
     $(el).attr('tag', element.hashtag);
     $(el).attr('count', element.count);
+    $(el).attr('leader', 'no');
+    $(el).attr('color', '0');
     leaderboard.append(el);
+
   } else {
     el = $('[tag=' + element.hashtag + ']');
     var count = Number($(el).attr('count'));
     $(el).attr('count', count + element.count);
   }
+  calculateRank();
+  giveColors();
+  takeColors();
+  reColor();
   sort();
+
+  // The new color
+  var color = $(el).attr('color');
+
+  pingLayer.ping([center.lng, center.lat], 'color_' + color);
+  logroll.prepend('<div class="logroll-item"><i>' +
+                  date + '</i> - ' +
+                  element.count + ' ' + element.feature + '(s) -' +
+                  '<span style="color: ' + colorMap[color] + ';">' +
+                  element.hashtag + '<span>' + '</div>');
 
   if (logroll.children().length > 100) {
     $('#logroll div:last-child').remove();
   }
+}
+
+function reColor () {
+  $('#leaderboard').children().each(function () {
+    $(this).css('color', colorMap[$(this).attr('color')]);
+  });
+}
+
+function takeColors () {
+  $('.need-color').each(function () {
+    var item = $(this);
+    var color = colors.pop();
+    item.attr('color', color);
+    item.removeClass('need-color');
+  });
+}
+
+function calculateRank () {
+  var ranks = [];
+  $('#leaderboard').children().each(function () {
+    ranks.push($(this));
+  });
+  ranks.sort(function (a, b) {
+    var a_count = Number(a.attr('count'));
+    var b_count = Number(b.attr('count'));
+    if (a_count < b_count) return 1;
+    if (a_count > b_count) return -1;
+    if (a_count === b_count) return 0;
+  }).forEach(function (el, index) {
+    el.attr('rank', index + 1);
+  });
+}
+
+function giveColors () {
+  $('#leaderboard').children().each(function () {
+    var item = $(this);
+    var isLeader = (item.attr('leader') === 'yes');
+    var isTopTen = Number(item.attr('rank')) <= 10;
+    var currentColor = item.attr('color');
+
+    // give back color
+    if (isLeader && !isTopTen) {
+      colors.push(currentColor);
+      item.attr('leader', 'no');
+      item.attr('color', '0');
+    }
+
+    if (!isLeader && isTopTen) {
+      item.addClass('need-color');
+      item.attr('leader', 'yes');
+    }
+  });
 }
 
 function sort () {
@@ -143,6 +198,9 @@ function sort () {
   tinysort('ul#leaderboard>li', {attr: 'count', order: 'desc'}).forEach(function (elm, i) {
     setTimeout((function (elm, i) {
       elm.style.top = i * liHeight + 'px';
+      var $elm = $(elm);
+      if (Number($elm.attr('rank')) < 10) { $elm.show(); }
+      else $elm.hide();
     }).bind(null, elm, i), 40);
   });
 }
